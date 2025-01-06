d95d609aacc4b08c9c5f5ad732ce2064b535a816
Ibrairsyad17 <ibrairsyad17@gmail.com>
2025-01-07 02:10:48 +0700

    feat(embed-dossier-mstr-react): consolidate exports and enhance type definitions
    - Introduced a new `exports.ts` file to centralize exports for various components, types, and utilities, improving module organization and accessibility.
    - Updated `index.tsx` to include exports from `exports.ts`, ensuring all relevant components and types are available for import.
    - Added the `Settings` type to the `types/index.ts` file, enhancing the type definitions for better integration and usability.
    
    This commit enhances the embed-dossier-mstr-react package by streamlining exports and improving type management, facilitating easier access to essential components and types.

---
+++ packages/embed-dossier-mstr-react/src/exports.ts
@@ -0,0 +1,49 @@
+export { Button } from "./components/Button";
+
+export {
+  MicroStrategySDK,
+  MicroStrategyDossierConfig,
+  MicroStrategyDossierConfigCustomAuthenticationType,
+  MicroStrategyDossier,
+  EmbeddingContexts,
+  EmbedLibraryPageConfig,
+  EmbedLibraryPage,
+  EmbedDossierConsumptionPageConfig,
+  EmbedBotConsumptionPageConfig,
+  EmbedReportPageConfig,
+  EmbedReportPage,
+  EmbedDossierConsumptionPage,
+  EmbedBotConsumptionPage,
+  Settings,
+} from "./types";
+
+export type {
+  DossierConsumptionSettings,
+  BotConsumptionSettings,
+} from "./types/settings";
+
+export type { EventTypes, EventHandler, EventHandlers } from "./types/events";
+
+export { EVENT_TYPE } from "./constants/eventType";
+
+export type {
+  FilterTypeInfoCalendar,
+  FilterTypeInfoMetricQualByValue,
+} from "./types/filter";
+
+export type {
+  TableOfContents,
+  DossierPage,
+  DossierChapter,
+} from "./types/navigation";
+
+export type {
+  DockedCommentAndFilter,
+  DockedTheme,
+  NavigationBar,
+  CustomUi,
+  OptionsFeature,
+  ShareFeature,
+  CurrentPage,
+  PageInfo,
+} from "./types/utils";
--- packages/embed-dossier-mstr-react/src/index.tsx
+++ packages/embed-dossier-mstr-react/src/index.tsx
@@ -1,2 +1,3 @@
 export { Button } from "./components/Button";
 export * from "./hooks";
+export * from "./exports";
