a9be63e6b034b5659e557b6f466377415bdace12
Ibrairsyad17 <ibrairsyad17@gmail.com>
2025-01-10 00:04:30 +0700

    feat(embed-dossier-mstr-react): add .hintrc, enhance dashboard embedding, and update dependencies
    - Introduced a new `.hintrc` file to configure hinting rules for the project.
    - Updated `pnpm-lock.yaml` to include new dependencies: `classnames` and `clsx` for improved styling options.
    - Refactored the main application page to replace the `Button` component with the `DashboardEmbed` component, enhancing the dashboard embedding experience.
    - Created a new `useCreateDashboard` hook for managing dashboard creation and state, improving the integration with the MicroStrategy SDK.
    - Updated TypeScript configuration in `tsconfig.app.json` to enable incremental builds for better performance.
    - Made minor adjustments to the `App.tsx` file for code consistency and improved accessibility.
    
    This commit enhances the embed-dossier-mstr-react package by adding configuration, improving dashboard embedding capabilities, and updating dependencies for better functionality.

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 
-import { Button } from "embed-dossier-mstr-react";
+import { DashboardEmbed } from "embed-dossier-mstr-react";
 
 export default function Page() {
   return (
     <>
-      <h1>Web</h1>
-      <Button>Boop</Button>
+      <h1>Example for Embed Dossier MSTR React</h1>
+      <DashboardEmbed
+        dossierUrl="https://demo.microstrategy.com/MicroStrategyLibrary/app/B7CA92F04B9FAE8D941C3E9B7E0CD754/27D332AC6D43352E0928B9A1FCAF4AB0"
+        config={{
+          navigationBar: {
+            enabled: true,
+          },
+        }}
+        style={{
+          width: "1000px",
+          height: "1200px",
+        }}
+      />
     </>
   );
 }
