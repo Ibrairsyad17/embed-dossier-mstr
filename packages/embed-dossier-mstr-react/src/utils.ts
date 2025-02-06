cb500879e77a117c9671bacba8cc7cca3eb2b6d6
Ibrairsyad17 <ibrairsyad17@gmail.com>
2025-02-06 21:27:08 +0700

    feat(embed-dossier-mstr-react): add authentication functions for MicroStrategy Dossier embedding

--- packages/embed-dossier-mstr-react/src/utils.ts
+++ packages/embed-dossier-mstr-react/src/utils.ts
@@ -8,4 +8,114 @@ const getInfoFromUrl = (url: string) => {
   return { serverUrl, serverUrlLibrary, libraryName, projectId, dossierId };
 };
 
-export { getInfoFromUrl };
+/**
+ * Authentication Functions for MicroStrategy Dossier Embedding according to the MicroStrategy Embedding SDK
+ */
+
+const getAuthToken = async ({
+  serverUrlLibrary,
+}: {
+  serverUrlLibrary: string;
+}) => {
+  const options = {
+    method: "GET",
+    credentials: "include" as RequestCredentials,
+    mode: "cors" as RequestMode,
+    headers: { "content-type": "application/json" },
+  };
+  let mstrToken = "";
+
+  try {
+    const response = await fetch(`${serverUrlLibrary}/api/auth/token`, options);
+    if (response.ok) {
+      const data = await response.json();
+      mstrToken = data.token;
+      return mstrToken;
+    }
+    return response.json().then((json) => console.log(json));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createAuthToken = async ({
+  serverUrlLibrary,
+  username,
+  password,
+  loginMode,
+}: {
+  serverUrlLibrary: string;
+  username: string;
+  password: string;
+  loginMode: "guest" | "standard" | "saml" | "oidc" | "ldap";
+}) => {
+  const standardBody = {
+    loginMode: 1,
+    username,
+    password,
+  };
+
+  const guestBody = {
+    loginMode: 8,
+  };
+
+  const ldapBody = {
+    loginMode: 16,
+    username,
+    password,
+  };
+
+  const options = {
+    method: "POST",
+    credentials: "include" as RequestCredentials,
+    mode: "cors" as RequestMode,
+    headers: { "content-type": "application/json" },
+    body:
+      loginMode === "standard"
+        ? JSON.stringify(standardBody)
+        : loginMode === "guest"
+          ? JSON.stringify(guestBody)
+          : JSON.stringify(ldapBody),
+  };
+
+  try {
+    const response = await fetch(`${serverUrlLibrary}/api/auth/login`, options);
+    if (response.ok) {
+      return response.headers.get("x-mstr-authtoken");
+    } else {
+      return await response.json().then((json) => console.log(json));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const login = async ({
+  serverUrlLibrary,
+  username,
+  password,
+  loginMode,
+}: {
+  serverUrlLibrary: string;
+  username: string;
+  password: string;
+  loginMode: "guest" | "standard" | "saml" | "oidc" | "ldap";
+}) => {
+  const authToken = await getAuthToken({ serverUrlLibrary }).catch((error) => {
+    console.error(error);
+  });
+
+  if (authToken) {
+    console.log("An existing login session has been found, logging in...");
+    return authToken;
+  }
+
+  return await createAuthToken({
+    serverUrlLibrary,
+    username,
+    password,
+    loginMode,
+  });
+};
+
+export { getInfoFromUrl, getAuthToken, createAuthToken };
