deca7b4ee8e2c5cd0b2dc1840d9ce3f1d251ae24
Ibrairsyad17 <ibrairsyad17@gmail.com>
2025-02-20 01:17:45 +0700

    feat(embed-dossier-mstr-react): update version to 1.0.1, add keywords, and enhance DossierChapter interface with navigation methods

--- apps/docs/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# embed-dossier-react-docs
-
-## 0.0.1
-
-### Patch Changes
-
-- Updated dependencies
-  - embed-dossier-mstr-react@1.0.0
--- packages/embed-dossier-mstr-react/src/types/navigation.ts
+++ packages/embed-dossier-mstr-react/src/types/navigation.ts
@@ -11,4 +11,12 @@ export interface DossierChapter {
   name: string;
   nodeKey: string;
   pages: DossierPage[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getNextChapter: () => any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getPrevChapter: () => any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getFirstPage: () => any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getLastPage: () => any;
 }
